8a3d0cb7c23e54ca6eda1253213823fffc26a4ca
bexultan <b.imanbek@atiprojects.kz>
2026-05-28 09:04:52 +0500

    Test for fill form

---
+++ src/test/java/RegistrationTests.java
@@ -0,0 +1,85 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTests extends TestBase{
+
+    @Test
+    void successfulFillFormTest() {
+        open("/automation-practice-form");
+// Ввод имени
+        $("[id=firstName]").setValue("Name");
+
+// Ввод фамилии
+        $("[id=lastName]").setValue("Surname");
+
+// Ввод email
+        $("[id=userEmail]").setValue("useremail@test.com");
+
+// Выбор пола
+        $("[id=gender-radio-1]").click();
+
+// Ввод номера телефона
+        $("[id=userNumber]").setValue("7075919711");
+
+// --------------------
+// Выбор даты рождения
+// --------------------
+// Открытие календаря
+        $("[id=dateOfBirthInput]").click();
+// Выбор месяца
+        $(".react-datepicker__month-select")
+                .selectOption("September");
+// Выбор года
+        $(".react-datepicker__year-select")
+                .selectOption("1997");
+// Выбор дня
+        $(".react-datepicker__day--011")
+                .click();
+
+//Выбор предмета
+        $("#subjectsInput")
+                .setValue("Math")
+                .pressEnter();
+
+// Выбор хобби
+        $("[id=hobbies-checkbox-1]").click();
+
+//Загрузка картинки
+        File file = new File("/Users/koyshikenmail.ru/Desktop/42dd34709012230a11aedf23987dde57.jpg");
+
+        $("#uploadPicture").uploadFile(file);
+
+        $("[id=currentAddress]").setValue("Test address 211");
+
+//Выбор штата
+        $("#state").scrollTo().shouldBe(Condition.visible).click();
+
+        $("#state")
+                .find(byText("NCR"))
+                .shouldBe(Condition.visible)
+                .click();
+        $("#state").click();
+
+//Выбор города
+        $("#city").click();
+        $("#city").find(byText("Delhi")).click();
+
+//Кнопка Submit
+        $("#submit").click();
+
+//Проверка заполненных полей
+        $(".table-responsive").shouldHave(text("Name Surname"));
+        $(".table-responsive").shouldHave(text("useremail@test.com"));
+        $(".table-responsive").shouldHave(text("7075919711"));
+        $(".table-responsive").shouldHave(text("Math"));
+        $(".table-responsive").shouldHave(text("NCR"));
+        $(".table-responsive").shouldHave(text("Delhi"));
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+    }
+
+    @AfterEach
+    void afterEach(){closeWebDriver();
+    }
+}
