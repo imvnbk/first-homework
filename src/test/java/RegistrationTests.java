66c0b5a3d5b12fccf63990a361afb2469fd6ab33
bexultan <b.imanbek@atiprojects.kz>
2026-05-28 13:27:28 +0500

    Test for fill form

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
@@ -78,8 +81,64 @@ public class RegistrationTests extends TestBase{
         $(".table-responsive").shouldHave(text("Name Surname"));
         $(".table-responsive").shouldHave(text("useremail@test.com"));
         $(".table-responsive").shouldHave(text("7075919711"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("11 September,1997"));
         $(".table-responsive").shouldHave(text("Math"));
         $(".table-responsive").shouldHave(text("NCR"));
         $(".table-responsive").shouldHave(text("Delhi"));
     }
+
+    @Test
+    void onlyRequiredFieldsFillTest(){
+        open("/automation-practice-form");
+        // Ввод имени
+        $("[id=firstName]").setValue("Name");
+
+        // Ввод фамилии
+        $("[id=lastName]").setValue("Surname");
+
+        // Выбор пола
+        $("[id=gender-radio-1]").click();
+
+        // Ввод номера телефона
+        $("[id=userNumber]").setValue("7075919711");
+        // --------------------
+        // Выбор даты рождения
+        // --------------------
+        // Открытие календаря
+        $("[id=dateOfBirthInput]").click();
+        // Выбор месяца
+        $(".react-datepicker__month-select")
+                .selectOption("September");
+        // Выбор года
+        $(".react-datepicker__year-select")
+                .selectOption("1997");
+        // Выбор дня
+        $(".react-datepicker__day--011")
+                .click();
+
+        //Кнопка Submit
+        $("#submit").scrollTo().click();
+
+        //Проверка заполненных полей
+        $(".table-responsive").shouldHave(text("Name Surname"));
+        $(".table-responsive").shouldHave(text("7075919711"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("11 September,1997"));
+    }
+
+    @Test
+    void emptyFieldsTest(){
+        WebDriver driver = new ChromeDriver();
+        open("/automation-practice-form");
+
+    // Нажимаем Submit без заполнения формы
+        $("#submit").scrollTo().click();
+
+    // Проверяем, что модальное окно не появилось
+        boolean isModalDisplayed =
+                !driver.findElements(By.id("example-modal-sizes-title-lg")).isEmpty();
+    }
+
+
 }
